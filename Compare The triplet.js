c8947bda294ccfa8030ea9d1ec719df3b0642b59
shafayet1372 <80921337+shafayet1372@users.noreply.github.com>
2021-03-19 01:47:35 +0600

    Add files via upload

---
+++ Compare The triplet.js
@@ -0,0 +1,57 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the compareTriplets function below.
+function compareTriplets(a, b) {
+
+
+
+
+var result=[0,0];
+	var alice=0,bob=0;
+for(var i=0;i<a.length ;i++){
+	if(a[i]>b[i]){
+		alice++;
+		result[0]=alice;
+	}else if(a[i]<b[i]){
+		bob++;
+		result[1]=bob;
+	}
+}
+return result;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const a = readLine().replace(/\s+$/g, '').split(' ').map(aTemp => parseInt(aTemp, 10));
+
+    const b = readLine().replace(/\s+$/g, '').split(' ').map(bTemp => parseInt(bTemp, 10));
+
+    const result = compareTriplets(a, b);
+
+    ws.write(result.join(' ') + '\n');
+
+    ws.end();
+}
